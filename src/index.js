ffccbb9bb4bf0c6b6e63859e14c44a28ae1ebc37
Joshua <j.blengino@yahoo.com>
2021-07-02 16:24:22 -0700

    pair-programming w/ Vince

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
+//document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+//});
+
+document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let newTask = e.target['new-task-description'].value
+  addTask(newTask)
+})
+
+function addTask(task) {
+  let li = document.createElement('li')
+  let ul = document.querySelector('#tasks')
+  let btn = document.createElement('button')
+
+  li.textContent = task
+  btn.textContent = 'X'
+
+  li.append(btn)
+  ul.append(li)
+  console.log('ADD TASK FUNC')
+
+  btn.addEventListener('click', () => {
+    li.remove()
+  })
+}
